9e5753264c35b82c2676043ab4ad6f17f630b0c0
Minakshi2005 <minakshidudhbade@gmail.com>
2025-01-23 22:36:44 +0530

    new project

---
+++ src/main/java/Ex220125/Lab001.java
@@ -0,0 +1,13 @@
+package Ex220125;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+        System.out.println(3+9);
+//        System.out.println("hello world");//single line comment
+       /* System.out.println( "hi");
+        System.out.println(4-2);*/ //multiline comment
+        System.out.println("Hi Minakshi");
+    }
+
+}

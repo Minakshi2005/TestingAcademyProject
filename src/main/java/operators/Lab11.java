53548e504fef87141d955808974f4dcc3c33bbb0
Minakshi2005 <minakshidudhbade@gmail.com>
2025-01-28 17:44:05 +0530

    operator practise

--- src/main/java/operators/Lab11.java
+++ src/main/java/operators/Lab11.java
@@ -7,8 +7,18 @@ public class Lab11 {
         boolean b = true;
 //        boolean c= true && true;
         boolean c= true || false;
-        System.out.println(c);
-        System.out.println(!b);
-        System.out.println(!c);
+//        System.out.println(c);
+//        System.out.println(!b);
+//        System.out.println(!c);
+
+        System.out.println(true && true);
+        System.out.println(true && false);
+        System.out.println(false && true);
+        System.out.println(false && false);
+
+        System.out.println(true || true);
+        System.out.println(true || false);
+        System.out.println(false || true);
+        System.out.println(false || false);
     }
 }
